017273b6f115af7b789688ebd9662b2a0d168f4f
navjot kaur <navu77196@gmail.com>
2025-04-21 17:39:14 +0530

    structure/alignment

--- src/components/JobSeekers/Jobheader.jsx
+++ src/components/JobSeekers/Jobheader.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import JobsOpportunities from '../../assets/images/JobsOpportunities.jpg';``
 export default function Jobheader() {
   return (
-    <div className="w-full flex flex-col md:flex-row">
+    <div className="w-full flex flex-col  justify-center mx-auto  max-w-7xl md:flex-row">
       <div className="w-full md:w-1/2 p-8 md:p-16 flex flex-col justify-center">
         <h1 className="text-5xl font-bold text-orange-500 mb-6">Jobs Opportunities</h1>
         
